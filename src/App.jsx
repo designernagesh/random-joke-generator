6331bf159809fa93cd678598094a2328c6985999
designernagesh <designer.nagesh@gmail.com>
2023-12-04 21:00:42 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <div className="container">
+        <h2 className="title">Random Joke Generator</h2>
+        <p className="sub-title">Click the button and observe the background color! 😊 </p>
+        <button>Change Background Color</button>
+      </div>
+    </>
+  )
+}
+
+export default App;
